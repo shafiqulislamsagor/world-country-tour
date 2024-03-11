39d6a9a3852e741927b8b937e2eb5d15832d09e8
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-03-11 22:24:17 +0600

    some tag added

--- src/component/Countrie.jsx
+++ src/component/Countrie.jsx
@@ -13,6 +13,8 @@ const Countrie = ({country}) => {
             <img src={flags.png} alt="" />
             <h2>Name: {common}</h2>
             <h5>Native: {official}</h5>
+            <h5>Native: {official}</h5>
+            <h5>Native: {official}</h5>
             <p>Country No: {idd.root}</p>
             <div>
                 <button onClick={handleClick}>{!click ? 'visit':'visited'}</button>
